324adeccbe106757d0e6a568c6849f284b2487e1
laravel777crew <pedro@777crew.com>
2024-05-27 12:12:07 -0500

    Defined the alternative email rules

--- cf7-custom-email.php
+++ cf7-custom-email.php
@@ -15,5 +15,67 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+add_action('wpcf7_before_send_mail', 'cf7_custom_email_routing');
 
 
+function cf7_custom_email_routing($contact_form) {
+
+    $form_id = $contact_form->id();
+
+    $submission = WPCF7_Submission::get_instance();
+
+    if ($submission) {
+        // Obtain Form Data
+        $posted_data = $submission->get_posted_data();
+
+        $alternative_email = '';
+
+        // Defined Rules
+        if ($form_id === '2ab79a7'){
+            $location = $posted_data['your-location'] ?? '';
+            $type = $posted_data['your-type'] ?? '';
+
+            /*
+             * Case 1:
+             * If you mark that you are not in Ireland,
+             * please contact me at mayra@elischools.com
+             */
+
+            if ($location === 'No'){
+
+                $alternative_email = 'mayra@elischools.com';
+
+            }else if ($location === 'Yes'){
+
+                /*
+                 * Case 2:
+                 * If you click YES you are in Ireland and YES you are an ELI or WST student,
+                 * you must go to hello@irelandemploymenthub.com
+                 */
+
+                if ($type === 'Yes'){
+                    $alternative_email = 'hello@irelandemploymenthub.com';
+                }
+
+                /*
+                 * Case 3:
+                 * If you click that you are in Ireland but are not an ELI or WST student,
+                 * please contact me at mayra@elischools.com
+                 */
+
+                if ($type === 'No'){
+                    $alternative_email = 'mayra@elischools.com';
+                }
+
+            }
+        }
+
+        if (!empty($alternative_email)) {
+            $mail = $contact_form->prop('mail');
+            $mail['recipient'] = $alternative_email;
+
+            // Save the changes
+            $contact_form->set_properties(['mail' => $mail]);
+        }
+    }
+}
